199583056196a3c2ae1148321061ef23ccfe0a89
Brad Westness <brad.westness@outlook.com>
2021-02-09 14:22:57 -0600

    2015-10

---
+++ Answers/2015/10.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Text;
+
+namespace advent.Answers._2015
+{
+    public class _10 : IAnswer
+    {
+        private readonly string _input;
+
+        public _10(string input)
+        {
+            _input = input?.Trim() ?? string.Empty;
+        }
+
+        public string Part1()
+        {
+            string result = _input;
+
+            for (var i = 0; i < 40; i++)
+            {
+                result = LookAndSay(result);
+            }
+
+            return $"Result length: {result.Length}";
+        }
+
+        public string Part2()
+        {
+            string result = _input;
+
+            for (var i = 0; i < 50; i++)
+            {
+                result = LookAndSay(result);
+            }
+
+            return $"Result length: {result.Length}";
+        }
+
+        private StringBuilder _sb = new StringBuilder();
+
+        private string LookAndSay(ReadOnlySpan<char> input)
+        {
+            _sb.Clear();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                var count = 1;
+                char? next = (i < input.Length - 1)
+                    ? input[i + 1]
+                    : null;
+
+                while (current == next && i < input.Length)
+                {
+                    i++;
+                    count++;
+                    next = (i < input.Length - 1)
+                        ? input[i + 1]
+                        : null;
+                }
+
+                _sb.Append($"{count}{current}");
+            }
+
+            return _sb.ToString();
+        }
+    }
+}
