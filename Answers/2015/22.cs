fe38c23db698837462f8f3687d726d9e5b8bb3a6
Brad Westness <brad.westness@outlook.com>
2021-02-27 14:03:40 -0600

    2015-22

--- Answers/2015/22.cs
+++ Answers/2015/22.cs
@@ -1,348 +1,237 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace advent.Answers._2015
 {
     public class _22 : IAnswer
     {
         private readonly Boss _boss;
-        private readonly IList<Spell> _spells = new List<Spell>
-        {
-            new("Magic Missile", 53, 4, 0, null),
-            new("Drain", 73, 2, 2, null),
-            new("Shield", 113, 0, 0, new(6, 0, 0, 0, 7)),
-            new("Poison", 173, 0, 0, new(6, 0, 3, 0, 0)),
-            new("Recharge", 229, 0, 0, new(5, 101, 0, 0, 0))
-        };
 
         public _22(Input input) => _boss = new(input);
 
         public string Part1()
         {
-            Test();
-
             var minSpent = int.MaxValue;
-            IEnumerable<string> history = null;
-            PlayGames(ref minSpent, ref history, new(50, 500), _boss.Clone());
+            var player = new Player(50, 500);
+            var boss = _boss.Clone();
+
+            PlayGames(ref minSpent, player, boss);
             return $"Minimum mana spent to win the fight: {minSpent}.";
         }
 
         public string Part2()
         {
             var minSpent = int.MaxValue;
-            IEnumerable<string> history = null;
-            PlayGames(ref minSpent, ref history, new(50, 500, turnHpCost: 1), _boss.Clone());
+            var player = new Player(50, 500, 1);
+            var boss = _boss.Clone();
+
+            PlayGames(ref minSpent, player, boss);
             return $"Minimum mana spent to win the fight: {minSpent}.";
         }
 
-        private void Test()
+        private void PlayGames(ref int minSpent, Player player, Boss boss)
         {
-            // test 1
-            var minSpent = int.MaxValue;
-            IEnumerable<string> history = null;
-            PlayGames(ref minSpent, ref history, new(10, 250), new(13, 8));
-            WriteResults("Test Game 1", history);
-
-            // test 2
-            minSpent = int.MaxValue;
-            history = null;
-            PlayGames(ref minSpent, ref history, new(10, 250), new(14, 8));
-            WriteResults("Test Game 2", history);
-        }
-
-        private void WriteResults(string heading, IEnumerable<string> history)
-        {
-            Console.WriteLine($"\n\n{heading}:");
-            foreach (var line in history)
+            foreach (var spellName in _spells.Keys)
             {
-                Console.WriteLine($"\t{line}");
-            }
-            Console.WriteLine("\n\n");
-        }
-
-        // Recursively play every possible spell that the player can afford
-        // as long as the player hasn't died, and the amount spent is less
-        // than the minimum winning amount
-        private void PlayGames(ref int minSpent, ref IEnumerable<string> history, Player player, Boss boss)
-        {
-            foreach (var spell in _spells)
-            {
-                if (spell.Mana > player.Mana || player.ActiveEffects.Any(y => y.Key == spell.Name))
+                if (!player.CanCast(spellName))
                 {
-                    // either the player can't afford this spell,
-                    // or its an effect that is already active
                     continue;
                 }
 
                 var newPlayer = player.Clone();
                 var newBoss = boss.Clone();
 
-                newPlayer.TakeTurn(newBoss, spell);
-                if (!newPlayer.IsDefeated & !newBoss.IsDefeated)
+                newPlayer.TakeTurn(newBoss, spellName);
+                newBoss.TakeTurn(newPlayer);
+
+                if (newBoss.IsDefeated && newPlayer.Spent < minSpent)
                 {
-                    newBoss.TakeTurn(newPlayer);
+                    minSpent = newPlayer.Spent;
                 }
 
-                if (newPlayer.Spent < minSpent)
+                if (!newPlayer.IsDefeated && !newBoss.IsDefeated && newPlayer.Spent < minSpent)
                 {
-                    if (newBoss.IsDefeated)
-                    {
-                        // the player won, so update the minimum spent and winning history
-                        minSpent = newPlayer.Spent;
-                        history = newPlayer.History;
-                    }
-                    else if (!newPlayer.IsDefeated)
-                    {
-                        // if both characters are still alive,
-                        // and we haven't outspent the minimum,                    
-                        // we need to keep following this branch
-                        PlayGames(ref minSpent, ref history, newPlayer, newBoss);
-                    }
+                    PlayGames(ref minSpent, newPlayer, newBoss);
                 }
             }
         }
 
-        record Effect(int Turns, int Mana, int Damage, int Heal, int Armor);
-
-        record Spell(string Name, int Mana, int Damage, int Heal, Effect Effect);
-
-        class Player
+        private class Player
         {
-            private readonly IList<string> _history = new List<string>();
-            private readonly IDictionary<string, Effect> _effects = new Dictionary<string, Effect>();
-
             public Player(int hitPoints, int mana, int turnHpCost = 0)
             {
                 HitPoints = hitPoints;
                 Mana = mana;
                 TurnHpCost = turnHpCost;
+
+                foreach (var spellName in _spells.Keys)
+                {
+                    Effects.Add(spellName, 0);
+                }
             }
 
-            public int HitPoints { get; private set; }
-            public int Mana { get; private set; }
-            public int Spent { get; private set; }
-            public int TurnHpCost { get; private set; }
-
-            public int Armor =>
-                ActiveEffects.Sum(e => e.Value.Armor);
-
-            public IEnumerable<KeyValuePair<string, Effect>> ActiveEffects =>
-                _effects.Where(e => e.Value.Turns > 0);
+            public int HitPoints { get; set; }
+            public int Mana { get; set; }
+            public int TurnHpCost { get; set; }
+            public int Armor { get; set; }
+            public int Spent { get; set; }
+            private readonly IDictionary<string, int> Effects = new Dictionary<string, int>();
 
             public bool IsDefeated =>
                 HitPoints <= TurnHpCost;
 
-            public IEnumerable<string> History => _history;
+            public bool CanCast(string spellName) =>
+                Effects[spellName] <= 1 &&
+                _spells[spellName].Cost <= Mana;
 
-            public void AddHistory(string item) => _history.Add(item);
-
-            public void TakeTurn(Boss boss, Spell spell)
+            public void TakeTurn(Boss boss, string spellName)
             {
-                if (IsDefeated || boss.IsDefeated)
-                {
-                    return;
-                }
+                var spell = _spells[spellName];
 
                 HitPoints -= TurnHpCost;
-                AddHistory("");
-                AddHistory($"-- Player turn --");
-                AddHistory($"- Player has {HitPoints} hit points, {Armor} armor, {Mana} mana");
-                AddHistory($"- Boss has {boss.HitPoints} hit points");
-                if (TurnHpCost > 0)
-                {
-                    AddHistory($"Turn costs player {TurnHpCost} hit points; {HitPoints} hit points remaining.");
-                }
-                ApplyEffects(boss);
-                CastSpell(boss, spell);
-            }
 
-            private void CastSpell(Boss boss, Spell spell)
-            {
-                if (IsDefeated || boss.IsDefeated)
-                {
-                    return;
-                }
-
-                Mana -= spell.Mana;
-                Spent += spell.Mana;
-                AddHistory($"Player casts {spell.Name} for {spell.Mana} mana; {Mana} mana remaining.");
-
-                if (spell.Heal > 0)
-                {
-                    HitPoints += spell.Heal;
-                    AddHistory($"{spell.Name} recovers {spell.Heal} hit points; {HitPoints} hit points remaining.");
-                }
-
-                if (spell.Damage > 0)
-                {
-                    boss.Defend(spell.Damage);
-                    AddHistory($"{spell.Name} deals {spell.Damage} damage; boss has {boss.HitPoints} hit points remaining.");
-                    if (boss.IsDefeated)
-                    {
-                        AddHistory($"This kills the boss, and the player wins.");
-                        return;
-                    }
-                }
-
-                if (spell.Effect?.Armor > 0)
-                {
-                    AddHistory($"{spell.Name} increases armor by {spell.Effect.Armor}.");
-                }
-
-                if (spell.Effect is not null)
-                {
-                    _effects.Add(spell.Name, spell.Effect);
-                }
-            }
-
-            public void Defend(Boss boss)
-            {
-                if (IsDefeated || boss.IsDefeated)
-                {
-                    return;
-                }
-
-                var armor = Armor;
-                var hp = Math.Max(1, boss.Damage - armor);
-                HitPoints -= hp;
-                AddHistory($"Boss attacks for {boss.Damage} - {armor} = {hp} damage; {HitPoints} hit points remaining.");
                 if (IsDefeated)
                 {
-                    AddHistory($"This kills the player! Game over.");
+                    return;
+                }
+
+                ApplyEffects(boss);
+
+                if (boss.IsDefeated)
+                {
+                    return;
+                }
+
+                Mana -= spell.Cost;
+                Spent += spell.Cost;
+                HitPoints += spell.Heal;
+                Effects[spellName] += spell.Turns;
+                boss.HitPoints -= spell.Damage;
+
+                if (spell.Start is object)
+                {
+                    spell.Start(this);
                 }
             }
 
             public void ApplyEffects(Boss boss)
             {
-                if (IsDefeated || boss.IsDefeated)
+                foreach (var spellName in Effects.Keys)
                 {
-                    return;
-                }
-
-                foreach (var key in _effects.Keys)
-                {
-                    if (_effects[key].Damage > 0)
+                    if (Effects[spellName] < 1)
                     {
-                        boss.Defend(_effects[key].Damage);
-                        AddHistory($"{key} deals {_effects[key].Damage} damage; boss has {boss.HitPoints} hit points remaining.");
+                        continue;
+                    }
+
+                    var spell = _spells[spellName];
+
+                    if (spell.Effect is object)
+                    {
+                        spell.Effect(this, boss);
+
                         if (boss.IsDefeated)
                         {
-                            AddHistory($"This kills the boss, and the player wins.");
                             return;
                         }
                     }
 
-                    if (_effects[key].Mana > 0)
-                    {
-                        Mana += _effects[key].Mana;
-                        AddHistory($"{key} provides {_effects[key].Mana} mana; {Mana} mana remaining.");
-                    }
+                    Effects[spellName]--;
 
-                    _effects[key] = _effects[key] with
+                    if (Effects[spellName] == 0 && spell.End is object)
                     {
-                        Turns = _effects[key].Turns - 1
-                    };
-
-                    if (_effects[key].Turns < 1)
-                    {
-                        _effects.Remove(key);
-                        AddHistory($"{key} wears off.");
-                    }
-                    else
-                    {
-                        AddHistory($"{key}'s timer is now {_effects[key].Turns}.");
+                        spell.End(this);
                     }
                 }
             }
 
             public Player Clone()
             {
-                Player clone = new(HitPoints, Mana, TurnHpCost);
+                var clone = new Player(HitPoints, Mana, TurnHpCost);
                 clone.Spent = Spent;
+                clone.Armor = Armor;
 
-                foreach (var item in _history)
+                foreach (var spellName in Effects.Keys)
                 {
-                    clone.AddHistory(item);
-                }
-
-                foreach (var key in _effects.Keys)
-                {
-                    clone._effects.Add(key, _effects[key] with { });
+                    clone.Effects[spellName] = Effects[spellName];
                 }
 
                 return clone;
             }
         }
 
-        class Boss
+        private class Boss
         {
             public Boss()
             {
 
             }
 
-            public Boss(int hitPoints, int damage)
-            {
-                HitPoints = hitPoints;
-                Damage = damage;
-            }
-
             public Boss(Input input)
             {
                 foreach (var line in input.ReadLines())
                 {
-                    var (key, value, _) = line.Split(":");
+                    var (property, value, _) = line.Split(": ");
+                    int.TryParse(value, out var intVal);
 
-                    switch (key.Replace(" ", ""))
+                    switch (property.Replace(" ", ""))
                     {
                         case nameof(HitPoints):
-                            HitPoints = int.Parse(value);
+                            HitPoints = intVal;
                             break;
 
                         case nameof(Damage):
-                            Damage = int.Parse(value);
+                            Damage = intVal;
                             break;
                     }
                 }
             }
 
-            public int HitPoints { get; private set; }
-            public int Damage { get; private set; }
+            public int HitPoints { get; set; }
+            public int Damage { get; set; }
 
             public bool IsDefeated =>
-                HitPoints < 1;
+                HitPoints <= 0;
 
             public void TakeTurn(Player player)
             {
-                if (IsDefeated || player.IsDefeated)
-                {
-                    return;
-                }
-
-                player.AddHistory("");
-                player.AddHistory($"-- Boss turn --");
-                player.AddHistory($"- Player has {player.HitPoints} hit points, {player.Armor} armor, {player.Mana} mana");
-                player.AddHistory($"- Boss has {HitPoints} hit points");
                 player.ApplyEffects(this);
-                player.Defend(this);
-            }
 
-            public void Defend(int damage)
-            {
                 if (IsDefeated)
                 {
                     return;
                 }
 
-                HitPoints -= damage;
+                var damage = Math.Max(1, Damage - player.Armor);
+                player.HitPoints -= damage;
             }
 
-            public Boss Clone() => new Boss
+            public Boss Clone()
             {
-                HitPoints = HitPoints,
-                Damage = Damage
-            };
+                var clone = new Boss();
+                clone.HitPoints = HitPoints;
+                clone.Damage = Damage;
+
+                return clone;
+            }
         }
+
+        private record Spell(
+            int Cost,
+            int Damage,
+            int Heal,
+            int Turns,
+            Action<Player> Start,
+            Action<Player, Boss> Effect,
+            Action<Player> End
+        );
+
+        private static readonly IDictionary<string, Spell> _spells = new Dictionary<string, Spell>
+        {
+            { "Magic Missile", new(53, 4, 0, 0, null, null, null) },
+            { "Drain", new(73, 2, 2, 0, null, null, null) },
+            { "Shield", new(113, 0, 0, 6, p => p.Armor += 7, null, p => p.Armor -= 7) },
+            { "Poison", new(173, 0, 0, 6, null, (_, b) => b.HitPoints -= 3, null) },
+            { "Recharge", new(229, 0, 0, 5, null, (p, _) => p.Mana += 101, null) }
+        };
     }
 }
