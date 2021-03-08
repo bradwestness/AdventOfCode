21a173ad2f6335c5bee264eb853753c2ba8f5ea4
Brad Westness <brad.westness@outlook.com>
2021-03-08 15:32:08 -0600

    2016-09

---
+++ Answers/2016/09.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using advent.Input;
+
+namespace advent.Answers._2016
+{
+    public class _09 : IPuzzleAnswer
+    {
+        private string _input;
+
+        public _09(IPuzzleInput input) => _input = input.ReadToEnd();
+
+        public string Part1()
+        {
+            var length = GetDecompressedLength(_input);
+            return $"Decompressed length: {length}.";
+        }
+
+        public string Part2()
+        {
+            var length = GetDecompressedLength(_input, allowRecursion: true);
+            return $"Decompressed length: {length}.";
+        }
+
+        private long GetDecompressedLength(ReadOnlySpan<char> input, bool allowRecursion = false)
+        {
+            long length = input.Length;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] != '(')
+                {
+                    continue;
+                }
+
+                var marker = input.Slice(i, input.Slice(i).IndexOf(')') + 1);
+                var xIndex = marker.IndexOf('x');
+                var numChars = int.Parse(marker.Slice(1, xIndex - 1));
+                var numRepeats = int.Parse(marker.Slice(xIndex + 1, marker.IndexOf(')') - xIndex - 1));
+                var dataLength = allowRecursion
+                    ? GetDecompressedLength(input.Slice(i + marker.Length, numChars), true)
+                    : numChars;
+
+                length += dataLength * numRepeats - numChars - marker.Length;
+                i += marker.Length + numChars - 1;
+            }
+
+            return length;
+        }
+    }
+}
